22fe9f36b86e01b25967290c2f052be5f116c120
taft31 <171927502@qq.com>
2015-10-11 13:52:48 +0800

    gg

---
+++ src/com/huayu/taft/Contronller/Login.java
@@ -0,0 +1,10 @@
+package com.huayu.taft.Contronller;
+
+/**
+ * Created by Administrator on 15-10-11.
+ */
+public class Login {
+    public static void main(String[] args) {
+        System.out.println("9999");
+    }
+}
